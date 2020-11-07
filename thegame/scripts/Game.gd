ac71f6717454d9b07b4f2e966bae51bd763a9cf7
Petra Baranski <info@progsource.de>
2020-11-07 10:50:52 +0100

    spawn laser gun moved into own func

--- thegame/scripts/Game.gd
+++ thegame/scripts/Game.gd
@@ -4,6 +4,7 @@ extends Node2D
 var laser_gun_op = load("res://scripts/ObjectPool.gd").new()
 export var laser_gun_left_pos : float = 0.0
 export var laser_gun_right_pos : float = 180.0
+const laser_gun_vertical_start_pos : float = 50.0
 
 var doors_op = load("res://scripts/ObjectPool.gd").new()
 
@@ -14,16 +15,26 @@ func _enter_tree():
 
 
 func _ready():
-	var gun = laser_gun_op.get_object()
-	if gun:
-		gun.rotation_degrees = 0.0
-		gun.position = Vector2(laser_gun_left_pos, 50.0)
-		$ObjectContainer.add_child(gun)
-	var gun_right = laser_gun_op.get_object()
-	if gun_right:
-		gun_right.rotation_degrees = 180.0
-		gun_right.position = Vector2(laser_gun_right_pos, 50.0)
-		$ObjectContainer.add_child(gun_right)
+	# warning-ignore:return_value_discarded
+	_add_laser_gun(true)
+	# warning-ignore:return_value_discarded
+	_add_laser_gun(false)
 
 #func _process(delta):
 #	pass
+
+# returns false if it failed
+func _add_laser_gun(var is_left : bool) -> bool:
+	var gun = laser_gun_op.get_object()
+	if not gun:
+		return false
+
+	if is_left:
+		gun.rotation_degrees = 0.0
+		gun.position = Vector2(laser_gun_left_pos, laser_gun_vertical_start_pos)
+	else:
+		gun.rotation_degrees = 180.0
+		gun.position = Vector2(laser_gun_right_pos, laser_gun_vertical_start_pos)
+
+	$ObjectContainer.add_child(gun)
+	return true
