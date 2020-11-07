46f43dbe7bc6ea167749f6a82b126cb22de50886
teemitzitrone <agata.raap@posteo.de>
2020-11-07 21:52:47 +0100

    remove print

--- thegame/scripts/player.gd
+++ thegame/scripts/player.gd
@@ -41,6 +41,5 @@ func _kill() -> void :
 func _on_speed_up(toogle):
 	if toogle:
 		max_speed = GameManager.gravity + GameManager.gravity * GameManager.speed_up_factor
-		print(max_speed)
 	else:
 		max_speed = GameManager.gravity
