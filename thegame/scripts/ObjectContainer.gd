bd45afc7cfd9f09a1750d429e029aba89ea456c2
Petra Baranski <info@progsource.de>
2020-11-07 20:28:34 +0100

    Added platforms

--- thegame/scripts/ObjectContainer.gd
+++ thegame/scripts/ObjectContainer.gd
@@ -1,8 +1,8 @@
 extends ParallaxBackground
 
 var speed : float = 20.0
-var last_created_object_distance : float = 70.0
-var object_distance : float = 60.0
+var last_created_object_distance : float = 110.0
+var object_distance : float = 100.0
 
 var laser_gun_op = load("res://scripts/ObjectPool.gd").new()
 export var laser_gun_left_pos : float = 0.0
@@ -11,8 +11,11 @@ const laser_gun_vertical_start_pos : float = 330.0
 
 #var doors_op = load("res://scripts/ObjectPool.gd").new()
 
+var platforms_op = load("res://scripts/ObjectPool.gd").new()
+
 func _init():
 	laser_gun_op.init_object_pool("res://scenes/LaserGun.tscn", 10)
+	platforms_op.init_object_pool("res://scenes/Platform.tscn", 3)
 
 
 func _process(delta):
@@ -22,19 +25,22 @@ func _process(delta):
 	
 	if last_created_object_distance > object_distance:
 		_spawn_object()
+		object_distance = GameManager.rng.randi_range(40, 140)
 		last_created_object_distance = 0.0
 
 
 func _spawn_object():
-	var rand = GameManager.rng.randi_range(0, 1) # left or right laser
+	var rand = GameManager.rng.randi_range(0, 100)
 	
-	match rand:
-		0:
-			# warning-ignore:return_value_discarded
-			_add_laser_gun(true)
-		1:
-			# warning-ignore:return_value_discarded
-			_add_laser_gun(false)
+	if rand < 35:
+		# warning-ignore:return_value_discarded
+		_add_laser_gun(true)
+	elif rand < 70:
+		# warning-ignore:return_value_discarded
+		_add_laser_gun(false)
+	else:
+		# warning-ignore:return_value_discarded
+		_add_platform()
 
 
 func _physics_process(_delta):
@@ -42,6 +48,8 @@ func _physics_process(_delta):
 	if collision:
 		if collision.collider.is_in_group("laserguns"):
 			_remove_laser_gun(collision.collider)
+		if collision.collider.is_in_group("platforms"):
+			_remove_platform(collision.collider)
 
 
 # returns false if it failed
@@ -71,3 +79,23 @@ func _remove_laser_gun(var gun) -> void:
 	gun.set_is_enabled(false)
 	$ParallaxLayer.remove_child(gun)
 	laser_gun_op.return_object(gun)
+
+
+func _add_platform() -> bool :
+	var platform = platforms_op.get_object()
+	if not platform:
+		return false
+	
+	var pos = Vector2()
+	pos.x = GameManager.rng.randi_range(30, 100)
+	pos.y = (-1 * scroll_offset.y) + laser_gun_vertical_start_pos
+	
+	platform.position = pos
+	
+	$ParallaxLayer.add_child(platform)
+	
+	return true
+
+func _remove_platform(var platform) -> void:
+	$ParallaxLayer.remove_child(platform)
+	platforms_op.return_object(platform)
---
+++ thegame/scripts/Platform.gd
@@ -0,0 +1,8 @@
+extends StaticBody2D
+
+
+var id : int = -1
+
+func _ready():
+	add_to_group("platforms")
+
