6162d91224c971092a6c9305d49023d174ef33e8
teemitzitrone <agata.raap@posteo.de>
2020-11-07 23:58:46 +0100

    spikes and platforms

--- thegame/scripts/Platform.gd
+++ thegame/scripts/Platform.gd
@@ -1,8 +1,16 @@
 extends StaticBody2D
 
-
 var id : int = -1
+
+onready var spikes = [$Spike1, $Spike2, $Spike3, $Spike4, $Spike5]
 
 func _ready():
 	add_to_group("platforms")
-
+	_add_spikes()
+	
+func _add_spikes():
+	if GameManager.rng.randf() <= 0.5:
+		return
+	
+	for spike in spikes:
+		spike.visible = true
