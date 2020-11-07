5179ebba7ccad8bcf5417388da1ad629c6b76948
teemitzitrone <agata.raap@posteo.de>
2020-11-08 00:29:20 +0100

    now with 50% more spikes

--- thegame/scripts/Platform.gd
+++ thegame/scripts/Platform.gd
@@ -6,6 +6,9 @@ onready var spikes = [$Spike1, $Spike2, $Spike3, $Spike4, $Spike5]
 
 func _ready():
 	add_to_group("platforms")
+	for spike in spikes:
+		spike.visible = false
+		spike.get_node("CollisionPolygon2D").set_deferred("disabled", true)
 	_add_spikes()
 	
 func _add_spikes():
@@ -14,3 +17,4 @@ func _add_spikes():
 	
 	for spike in spikes:
 		spike.visible = true
+		spike.get_node("CollisionPolygon2D").set_deferred("disabled", false)
