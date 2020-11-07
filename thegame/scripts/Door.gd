3b9b4f9c43ef496c8f3168ab0697ee058884374f
Petra Baranski <info@progsource.de>
2020-11-07 22:17:57 +0100

    Stop doors on timer stop

--- thegame/scripts/Door.gd
+++ thegame/scripts/Door.gd
@@ -6,4 +6,12 @@ var id : int = -1
 func _ready():
 	add_to_group("doors")
 	$AnimationPlayer.play("move")
+	
+	# warning-ignore:return_value_discarded
+	GameManager.connect("stop_time", self, "_on_stop_time_triggered")
 
+func _on_stop_time_triggered(toogle):
+	if toogle:
+		$AnimationPlayer.stop(false)
+	else:
+		$AnimationPlayer.play("move")
