5a6c5b1672c196beabe72115b8f07d9519318d72
teemitzitrone <agata.raap@posteo.de>
2020-11-08 13:51:15 +0100

    center of gravity

--- thegame/scripts/Game.gd
+++ thegame/scripts/Game.gd
@@ -2,6 +2,7 @@ extends Node2D
 
 onready var player := $Player
 onready var popup := $HUD/Popup
+onready var optimal_position : Vector2 = $OtimalPosition.position
 
 func _ready():
 	# warning-ignore:return_value_discarded
@@ -21,3 +22,13 @@ func _unpause():
 	player.set_process_input(true)
 	GameManager.set_process_unhandled_input(true)
 	get_tree().paused = false
+
+func _physics_process(delta):
+	if player.check_collision() or player.position.y == optimal_position.y:
+		return
+		
+	if player.position.distance_to(optimal_position) > 5:
+		var _velocity = Vector2(0.0, position.direction_to(optimal_position).y) * GameManager.gravity
+		_velocity = player.move_and_slide(_velocity)
+	else:
+		player.position.y = optimal_position.y
