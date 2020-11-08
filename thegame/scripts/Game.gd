1299accfc85717919061488cfa0baf9d464c246a
Petra Baranski <info@progsource.de>
2020-11-08 14:06:08 +0100

    Added die sound

--- thegame/scripts/Game.gd
+++ thegame/scripts/Game.gd
@@ -3,21 +3,28 @@ extends Node2D
 onready var player := $Player
 onready var popup := $HUD/Popup
 
+var is_paused = false
+
 func _ready():
 	# warning-ignore:return_value_discarded
 	GameManager.connect("kill", self, "_on_kill")
+
 	_unpause()
 
 func _on_kill():
+	if not is_paused:
+		popup.die_sound.play(0.5)
 	_pause()
 	popup.popup()
 	
 func _pause():
+	is_paused = true
 	player.set_process_input(false)
 	GameManager.set_process_unhandled_input(false)
 	get_tree().paused = true
 	
 func _unpause():
+	is_paused = false
 	player.set_process_input(true)
 	GameManager.set_process_unhandled_input(true)
 	get_tree().paused = false
--- thegame/scripts/Popup.gd
+++ thegame/scripts/Popup.gd
@@ -1,5 +1,7 @@
 extends Popup
 
+onready var die_sound = $DieSound
+
 func _on_Main_Menu_pressed():
 	# warning-ignore:return_value_discarded
 	get_tree().change_scene("res://scenes/StartScreen.tscn")
